041271854211b3b287af8f91b308fe9339e2fe84
Ayana Bando <ayanaflax@yahoo.com>
2018-10-01 23:30:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,13 @@ class App < Sinatra::Base
   end
   
   post '/team' do
-    @name = params[:name].join(''\n')
-    @coach = params[:coach].join(''\n')
-    @pg = params[:pg].join(''\n')
-    @sg = params[:sg].join(''\n')
-    @pf = params[:pf].join(''\n')
-    @sf = params[:sf].join(''\n')
-    @c = params[:c].join(''\n')
+    @name = params[:name].join('\n')
+    @coach = params[:coach].join(\n')
+    @pg = params[:pg].join('\n')
+    @sg = params[:sg].join(\n')
+    @pf = params[:pf].join(\n')
+    @sf = params[:sf].join(\n')
+    @c = params[:c].join('\n')
     erb :team
   end
 end
