397717670135a18a2398c4e4cb1b2f9dc716fc86
Ayana Bando <ayanaflax@yahoo.com>
2018-10-01 23:13:45 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,6 +1,17 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
-
+  get '/' do
+    erb :index
+  end
+  
+  get '/newteam' do
+    erb :newteam
+  end
+  
+  post '/team' do
+    erb :team
+     @member = [coach == 'Louis Armstrong', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+  end
 
 end
